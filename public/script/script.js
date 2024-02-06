3ec671612e13526e3884dea7beb371e011591207
DamianR04 <damian.romeijn@hva.nl>
2024-02-06 20:35:47 +0100

    v.07

--- public/script/script.js
+++ public/script/script.js
@@ -1,6 +1,24 @@
-const expandButton = document.querySelector('header button')
-expandButton.addEventListener('click', expand)
+let innerCursor = document.querySelector(".inner-cursor");
+let outerCursor = document.querySelector(".outer-cursor");
 
-function expand () {
-  document.body.classList.toggle('expand')
+document.addEventListener("mousemove", moveCursor);
+
+function moveCursor(e) {
+  let x = e.clientX;
+  let y = e.clientY;
+innerCursor.style.left = `${x}px`;
+innerCursor.style.top = `${y}px`;
+outerCursor.style.left = `${x}px`;
+outerCursor.style.top = `${y}px`;
 }
+
+let text = Array.from(document.querySelectorAll("h1, p, h2, h3, a"));
+
+text.forEach((tekst) => { 
+  tekst.addEventListener("mouseover", () => {
+  innerCursor.classList.add("grow");
+  });
+    tekst.addEventListener("mouseleave", () => { 
+    innerCursor.classList.remove("grow");
+    });
+});
