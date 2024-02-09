218cdd6944bea8fae77e00459ab2f8e1bab78093
DamianR04 <damian.romeijn@hva.nl>
2024-02-09 13:04:24 +0100

    fix comments in JS close #5
    Is opgelost, bedankt!

--- public/script/script.js
+++ public/script/script.js
@@ -1,7 +1,13 @@
+// Haalt mijn classes op uit CSS //
+
 let innerCursor = document.querySelector(".inner-cursor");
 let outerCursor = document.querySelector(".outer-cursor");
 
+// Hier heb ik een mousemove event aangeroepen //
+
 document.addEventListener("mousemove", moveCursor);
+
+// Eerst heb ik de x en y as aangeroepen. Zo werkt het event als er over x of y as gehoverd wordt//
 
 function moveCursor(e) {
   let x = e.clientX;
@@ -11,6 +17,8 @@ innerCursor.style.top = `${y}px`;
 outerCursor.style.left = `${x}px`;
 outerCursor.style.top = `${y}px`;
 }
+
+// Hier heb ik via een array al mijn elementen waar de muis groter moet worden neergezet//
 
 let text = Array.from(document.querySelectorAll("h1, h2, h3, a, .image"));
 
